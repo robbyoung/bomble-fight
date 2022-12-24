3434b33e424b0cc93315fac15d6074d859219d0c
Robert Young <robertyoung10101@gmail.com>
2022-12-24 13:39:33 +1300

    Create an addBet aggregate method

--- apps/api/internal/fights/aggregate.go
+++ apps/api/internal/fights/aggregate.go
@@ -1,5 +1,7 @@
 package fights
 
+import "errors"
+
 type aggregate struct {
 	bets         []bet
 	playerIds    []string
@@ -20,4 +22,44 @@ func newAggregate(players []string, combatants []string) *aggregate {
 		combatantIds: combatants,
 		status:       Pending,
 	}
+}
+
+func (agg *aggregate) AddBet(playerId string, combatantId string, amount int) error {
+	if !agg.ContainsPlayer(playerId) {
+		return errors.New("no record of specified player")
+	}
+
+	if !agg.ContainsCombatant(combatantId) {
+		return errors.New("no record of specified combatant")
+	}
+
+	if amount <= 0 {
+		return errors.New("bet amount must be greater than 0")
+	}
+
+	b := bet{
+		PlayerId:    playerId,
+		CombatantId: combatantId,
+		Amount:      amount,
+	}
+	agg.bets = append(agg.bets, b)
+	return nil
+}
+
+func (agg *aggregate) ContainsPlayer(id string) bool {
+	for _, p := range agg.playerIds {
+		if p == id {
+			return true
+		}
+	}
+	return false
+}
+
+func (agg *aggregate) ContainsCombatant(id string) bool {
+	for _, c := range agg.combatantIds {
+		if c == id {
+			return true
+		}
+	}
+	return false
 }
